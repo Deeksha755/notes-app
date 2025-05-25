1fbcae02686d08f4a85f68ea0a6a30199a7d4e68
Deeksha Chaudhary <chaudharydeeksha755@gmail.com>
2025-05-25 09:39:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-const toggles = document.querySelectorAll(".toggle");
-const good = document.getElementById("good");
-const cheap = document.getElementById("cheap");
-const fast = document.getElementById("fast");
+const addButton = document.getElementById("add");
+const notes = JSON.parse(localStorage.getItem("notes"));
 
-const doTheTrick = (theClickedOne) => {
-  if (good.checked && cheap.checked && fast.checked) {
-    if (good === theClickedOne) fast.checked = false;
-    if (cheap === theClickedOne) good.checked = false;
-    if (fast === theClickedOne) cheap.checked = false;
-  }
+const updateLocalStorage = () => {
+  const notesText = document.querySelectorAll("textarea");
+  const notes = [];
+  notesText.forEach((note) => notes.push(note.value));
+  localStorage.setItem("notes", JSON.stringify(notes));
 };
 
-toggles.forEach((toggle) =>
-  toggle.addEventListener("change", (e) => doTheTrick(e.target))
-);
+const addNewNote = (text = "") => {
+  const note = document.createElement("div");
+  note.classList.add("note");
+  note.innerHTML = `
+  <div class="tools">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+  </div>
+  <div class="main ${text ? "" : "hidden"}"></div>
+  <textarea class="${text ? "hidden" : ""}"></textarea>`;
+
+  const editButton = note.querySelector(".edit");
+  const deleteButton = note.querySelector(".delete");
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+  textArea.value = text;
+  main.innerHTML = marked(text);
+
+  deleteButton.addEventListener("click", () => {
+    note.remove();
+    updateLocalStorage();
+  });
+  editButton.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+  textArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+    main.innerHTML = marked(value);
+    updateLocalStorage();
+  });
+  document.body.appendChild(note);
+};
+
+addButton.addEventListener("click", () => addNewNote());
+
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
+}
